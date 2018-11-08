0cffa4df695238a2b042a0aaf345f39bfe4d5f36
pjbielec <przemyslaw.bielec@core3.pl>
2018-11-08 16:03:06 +0100

    Added editor

---
+++ src/main/java/vaadin/com/vaadinpostgres/CompanyEditor.java
@@ -0,0 +1,10 @@
+package vaadin.com.vaadinpostgres;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+@SpringComponent
+@UIScope
+public class CompanyEditor extends VerticalLayout {
+}
